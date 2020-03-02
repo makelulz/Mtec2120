f19b328ffee4bc0df811fadd42cd25529c98b9b8
Unknown <jared.holness@gmail.com>
2020-03-02 13:44:49 -0500

    Added Class Work for 3-2-2020

---
+++ 3-2-2020/3-2-2020/Assets/BigBoi.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBoi : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool allowControl = true;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (allowControl == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.localScale += Vector3.one * Time.deltaTime;
+            }
+        }
+
+        if (transform.localScale.x > 5)
+        {
+            allowControl = false;
+
+        }
+
+        if (allowControl == false)
+        {
+            transform.localScale -= Vector3.one * Time.deltaTime;
+        }
+
+        if (transform.localScale.x < 1)
+        {
+            allowControl = true;
+        }
+    }
+}
